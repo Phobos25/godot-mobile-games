4ef4e4b54061dc20e67e140a83f6cc72a45481b5
Home\igor <fydte@mail.ru>
2025-07-23 11:19:14 +0900

    -add knife hitting the target

--- elements/targets/target/target.gd
+++ elements/targets/target/target.gd
@@ -1,8 +1,20 @@
 extends CharacterBody2D
+class_name Target
 
-var speed := PI
+const KNIFE_POSITION = Vector2(0, 180)
+
 # 2 PI is full circle -> 1 PI is half circle
+var speed := PI
+
+#container to hold knifes
+@onready var items_container := $ItemsContainer
 
 func _physics_process(delta: float):
 	rotation += speed*delta
 
+# function to rotate knifes. object_rotation is angle in radians
+func add_object_with_pivot(object: Node2D, object_rotation: float):
+	var pivot := Node2D.new()
+	pivot.rotation = object_rotation
+	pivot.add_child(object)
+	items_container.add_child(pivot)
